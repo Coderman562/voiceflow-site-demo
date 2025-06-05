e9bd28941c21fef20e0789cdb5ef732822c3690e
Coderman562 <126994465+Coderman562@users.noreply.github.com>
2025-06-04 19:08:19 -0700

    change to milpitas charity bingo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,16 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>AquaMaids Bingo Demo</title>
+        <title>Milpitas Charity Bingo Demo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       {/* Embed the external site */}
       <iframe
-        src="https://www.aquamaidsbingo.com/index.html"
+        src="https://milpitascharitybingo.com/"
         style={{
           width: "100%",
           height: "100vh",
-          border: 0,
         }}
       />
 
